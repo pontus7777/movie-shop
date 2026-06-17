bb979a1caefbdf70c20bccdbdc8a96c4ad91b2c8
grosseRiese <plmman2010@gmail.com>
2026-06-17 11:49:59 +0200

    add admin folder -start-

---
+++ app/(admin)/admin/movies/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreatePage() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="mx-auto text-4xl font-bold">Create!</h1>
+      </main>
+    </div>
+  );
+}
