e05380859496633cdb5e736555e4fa6d608079fb
pontus7777 <28758232+pontus7777@users.noreply.github.com>
2026-06-17 16:21:23 +0200

    styled sidebar slightly

--- app/(admin)/admin/_components/admin-sidebar.tsx
+++ app/(admin)/admin/_components/admin-sidebar.tsx
@@ -1,26 +1,39 @@
 import { ThemeModeToggle } from '@/components/theme-mode-toggle';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
   SidebarHeader,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 export function AdminSidebar() {
   return (
     <Sidebar>
       <SidebarHeader>
-        <h1 className="space-y-4 text-2xl font-bold">Header</h1>
+        <h1 className="space-y-4 text-2xl font-bold">Dashboard</h1>
       </SidebarHeader>
       <SidebarContent>
-        <p>Content</p>
+        <SidebarGroup>
+          <Button asChild>
+            <Link href={'/admin/create'}>Add Movie</Link>
+          </Button>
+        </SidebarGroup>
       </SidebarContent>
 
       <SidebarFooter>
-        <div>
-          <p>Footer</p>
-          <ThemeModeToggle />
-        </div>
+        <SidebarGroup>
+          {/* Maybe a Admin profile */}
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p>Logged in Admin</p>
+        </SidebarGroup>
+        <ThemeModeToggle />
       </SidebarFooter>
     </Sidebar>
   );
