de0f32b33cbe5be169ce2887643aff68cf51678e
pontus7777 <28758232+pontus7777@users.noreply.github.com>
2026-06-24 13:49:02 +0200

    some actions and the page + table

---
+++ app/(admin)/admin/genres/_actions/create-genre-action.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import prisma from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+
+const createGenreSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+})
+
+export async function createGenre(values: z.infer<typeof createGenreSchema>) {
+  const data = createGenreSchema.parse(values)
+  try {
+    const newGenre = prisma.genre.create({
+      data: {
+        name: data.name,
+        description: data.name,
+      },
+    })
+
+    revalidatePath(`/admin/genres`)
+
+    return newGenre
+  } catch (error) {
+    console.log('Error creating a genre', error)
+    throw new Error('Faild to create a genre: ')
+  }
+}
