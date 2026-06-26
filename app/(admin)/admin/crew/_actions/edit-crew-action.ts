cd36fb55b9b15391883df8f763b7c329e3d238d4
grosseRiese <plmman2010@gmail.com>
2026-06-26 14:01:19 +0200

    Create edit-crew-action.ts

---
+++ app/(admin)/admin/crew/_actions/edit-crew-action.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+
+import prisma from '@/lib/prisma'
+
+const editActorSchema = z.object({
+  id: z.string().min(1),
+  name: z.string().min(1),
+  role: z.string().min(1).max(50),
+  movieIds: z.array(z.string()).optional(),
+})
+
+export async function editCrew(values: z.infer<typeof editActorSchema>) {
+  const data = editActorSchema.parse(values)
+
+  const updatedActor = await prisma.crew.update({
+    where: {
+      id: data.id,
+      role: { equals: 'ACTOR' },
+    },
+    data: {
+      name: data.name,
+    },
+
+    // IF YOU WANT MOVIES
+    // include: {
+    //   movies: true,
+    // },
+  })
+
+  revalidatePath('/admin/crew')
+
+  return updatedActor
+}
