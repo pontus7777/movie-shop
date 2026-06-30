49e30a3da29f5bfbdb5f23ad43862f5ce7639d57
grosseRiese <plmman2010@gmail.com>
2026-06-30 14:49:58 +0200

    Update movie-crew-editor.tsx

--- components/movies/movie-crew-editor.tsx
+++ components/movies/movie-crew-editor.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
-import { Trash2 } from 'lucide-react'
 import {
   Select,
   SelectContent,
