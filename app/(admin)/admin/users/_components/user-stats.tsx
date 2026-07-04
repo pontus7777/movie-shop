96b2268e7983a380b2c9e7ec0fe6baa3f5170930
Grosse Riese <plmman2010@gmail.com>
2026-07-04 10:42:54 +0200

    Update user-stats.tsx

--- app/(admin)/admin/users/_components/user-stats.tsx
+++ app/(admin)/admin/users/_components/user-stats.tsx
@@ -5,56 +5,46 @@ type Props = {
   totalUsers: number
   newUsers: number
   admins: number
-  verifiedUsers?: number
+  verifiedUsers: number
 }
 
-export function StatsTable({ totalUsers, admins, verifiedUsers, newUsers }: Props) {
+export function UserStats({ totalUsers, admins, verifiedUsers, newUsers }: Props) {
+  const stats = [
+    {
+      title: 'Total Users',
+      value: totalUsers,
+      icon: Users,
+    },
+    {
+      title: 'Administrators',
+      value: admins,
+      icon: ShieldUser,
+    },
+    {
+      title: 'Verified Users',
+      value: verifiedUsers,
+      icon: UserCheck,
+    },
+    {
+      title: 'New This Month',
+      value: newUsers,
+      icon: LayersPlus,
+    },
+  ]
+
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription className="flex items-center justify-between">
-            <span>Total Users</span>
-            <Users className="h-4 w-4 text-muted-foreground" />
-          </CardDescription>
-
-          <CardTitle className="text-3xl">{totalUsers}</CardTitle>
-        </CardHeader>
-      </Card>
-
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription className="flex items-center justify-between">
-            <span>Administrators</span>
-            <ShieldUser className="h-4 w-4 text-muted-foreground" />
-          </CardDescription>
-
-          <CardTitle className="text-3xl">{admins}</CardTitle>
-        </CardHeader>
-      </Card>
-
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription className="flex items-center justify-between">
-            <span>Active Users</span>
-            <UserCheck className="h-4 w-4 text-muted-foreground" />
-          </CardDescription>
-          <CardTitle className="text-3xl">
-            {/* {users.filter((u) => u.emailVerified).length} */}
-            {verifiedUsers}
-          </CardTitle>
-        </CardHeader>
-      </Card>
-
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription className="flex items-center justify-between">
-            <span>New This Month</span>
-            <LayersPlus className="h-4 w-4 text-muted-foreground" />
-          </CardDescription>
-          <CardTitle className="text-3xl">{newUsers}</CardTitle>
-        </CardHeader>
-      </Card>
+      {stats.map((stat) => (
+        <Card key={stat.title}>
+          <CardHeader className="pb-2">
+            <CardDescription className="flex items-center justify-between">
+              <span>{stat.title}</span>
+              <stat.icon className="h-4 w-4 text-muted-foreground" />
+            </CardDescription>
+          </CardHeader>
+          <CardTitle className="text-3xl">{stat.value}</CardTitle>
+        </Card>
+      ))}
     </div>
   )
 }
