3c27d0f7749a9613dd142cc18c1b1b4d79d95939
Grosse Riese <plmman2010@gmail.com>
2026-07-08 23:26:30 +0200

    Update get-orders-action.ts

--- app/(admin)/admin/orders/_actions/get-orders-action.ts
+++ app/(admin)/admin/orders/_actions/get-orders-action.ts
@@ -1,16 +1,69 @@
 'use server'
 
+import { Prisma, OrderStatus, PaymentMethod } from '@/generated/prisma/client'
 import prisma from '@/lib/prisma'
 
 const PAGE_SIZE = 10
 
-export async function getOrders(page = 1) {
+type GetOrdersParams = {
+  page?: number
+  search?: string
+  status?: string
+  payment?: string
+}
+
+export async function getOrders({ page = 1, search, status, payment }: GetOrdersParams) {
   const skip = (page - 1) * PAGE_SIZE
+
+  const where: Prisma.OrderWhereInput = {
+    AND: [
+      search
+        ? {
+            OR: [
+              {
+                id: {
+                  contains: search,
+                  mode: 'insensitive',
+                },
+              },
+              {
+                user: {
+                  name: {
+                    contains: search,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+              {
+                user: {
+                  email: {
+                    contains: search,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+            ],
+          }
+        : {},
+
+      status && status !== 'all'
+        ? {
+            status: status.toUpperCase() as OrderStatus,
+          }
+        : {},
+      payment
+        ? {
+            paymentMethod: payment.toUpperCase() as PaymentMethod,
+          }
+        : {},
+    ],
+  }
 
   const [orders, total] = await prisma.$transaction([
     prisma.order.findMany({
       skip,
       take: PAGE_SIZE,
+      where,
       orderBy: {
         createdAt: 'desc',
       },
