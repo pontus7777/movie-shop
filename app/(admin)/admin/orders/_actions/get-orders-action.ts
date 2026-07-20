909501f51402acfa1d06e70a981fe05f464fd679
grosseRiese <plmman2010@gmail.com>
2026-07-20 16:50:35 +0200

    Update get-orders-action.ts

--- app/(admin)/admin/orders/_actions/get-orders-action.ts
+++ app/(admin)/admin/orders/_actions/get-orders-action.ts
@@ -61,28 +61,6 @@ export async function getOrders({ page = 1, search, status, payment }: GetOrders
     ],
   }
 
-  // const [orders, total] = await prisma.$transaction([
-  //   prisma.order.findMany({
-  //     skip,
-  //     take: PAGE_SIZE,
-  //     where,
-  //     orderBy: {
-  //       createdAt: 'desc',
-  //     },
-  //     include: {
-  //       user: true,
-  //       items: {
-  //         include: {
-  //           movie: true,
-  //         },
-  //       },
-  //       shippingAddress: true,
-  //     },
-  //   }),
-
-  //   prisma.order.count(),
-  // ])
-
   const orders = await prisma.order.findMany({
     skip,
     take: PAGE_SIZE,
