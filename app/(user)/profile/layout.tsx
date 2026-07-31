f197d26961c584baa3ce7b98762b3a4a61563c1e
grosseRiese <plmman2010@gmail.com>
2026-07-31 09:52:14 +0200

    Update layout.tsx

--- app/(user)/profile/layout.tsx
+++ app/(user)/profile/layout.tsx
@@ -1,21 +1,16 @@
 import '@/app/globals.css'
 import { requireAuth } from '@/lib/session-validation'
 import Header from '@/app/(public)/_components/header/header'
-import { auth } from '@/lib/auth'
-import { headers } from 'next/headers'
 
 export default async function UserPagesLayout({ children }: { children: React.ReactNode }) {
   await requireAuth()
 
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
 
   return (
     <div className="bg-background flex min-h-screen w-full">
       <main className="flex flex-1 flex-col">
         {/* Header */}
-        <Header userName={session?.user.name ?? null} />
+        <Header />
 
         {/* Page content */}
         <div className="flex-1 px-6 py-8">{children}</div>
