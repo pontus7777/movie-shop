89d9baa0a3e2ac74e7d82e5eb2c55010dae34b38
grosseRiese <plmman2010@gmail.com>
2026-07-31 09:52:12 +0200

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,22 +3,18 @@ import '@/app/globals.css'
 import Footer from '@/app/(public)/_components/footer'
 import Header from '@/app/(public)/_components/header/header'
 
-import { auth } from '@/lib/auth'
-import { headers } from 'next/headers'
+import { requireAuth } from '@/lib/session-validation'
 
 export default async function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
-
+  // await requireAuth()
 
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <Header userName={session?.user.name ?? null} />
+      <Header />
 
       <main className="flex-1 overflow-x-hidden">{children}</main>
 
